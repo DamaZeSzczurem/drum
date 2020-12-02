91acfe6cb4e0e54f011e351af7ec57a1f74c51d5
DamaZeSzczurem <czarna.kawe.prosze@gmail.com>
2020-12-02 16:10:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var drumButton = document.getElementsByClassName("drum");
+
+function buttonAnimation(currentKey){
+    var activeButton= document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(
+      function(){
+        activeButton.classList.remove("pressed");
+      }, 500
+    )
+}
+
+function play(keyName){
+      switch (keyName) {
+        case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+          break;
+
+        case "a":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+            break;
+
+        case "s":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+            break;
+
+        case "d":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+            break;
+
+        case "j":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+            break;
+
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+              break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+             break;
+
+        default: console.log("there isn't any sound");
+      }
+}
+
+
+
+document.addEventListener("keydown", function(event){
+  play(event.key);
+  buttonAnimation(event.key);
+})
+
+for(var i=0; i<drumButton.length; i++){
+    drumButton[i].addEventListener("click",function(){
+      play(this.innerHTML);
+      buttonAnimation(this.innerHTML);
+    })
+  }
